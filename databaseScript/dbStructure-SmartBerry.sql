a7f94c8709a9d97efc4defb03e0b9b5cd4138e1f
davi-domingues <davi.domingues@sptech.school>
2025-04-05 14:56:27 -0300

    Criando script principal da estrutura do bd (s/ tabelas criadas)

---
+++ databaseScript/dbStructure-SmartBerry.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE SmartBerry;
+
+-- Criar Tabela 'SmartBerry.Endereco' de acordo com MER
+
+-- Criar Tabela 'SmartBerry.Cliente' de acordo com MER 
+
+-- Criar Tabela 'SmartBerry.Sensor' de acordo com MER
+
+-- Criar Tabela 'SmartBerry.Registro' de acordo com MER
