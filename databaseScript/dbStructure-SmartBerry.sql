6e31d489d241c8d67cc45c75abcba9a26f9401b8
davi-domingues <davi.domingues@sptech.school>
2025-04-10 12:45:44 -0300

    Corrigindo erros na criação da estrutura do banco

--- databaseScript/dbStructure-SmartBerry.sql
+++ databaseScript/dbStructure-SmartBerry.sql
@@ -1,12 +1,5 @@
 CREATE DATABASE SmartBerry;
 USE SmartBerry;
--- Criar Tabela 'SmartBerry.Endereco' de acordo com MER
-
--- Criar Tabela 'SmartBerry.Cliente' de acordo com MER 
-
--- Criar Tabela 'SmartBerry.Sensor' de acordo com MER
-
--- Criar Tabela 'SmartBerry.Registro' de acordo com MER
 
 CREATE TABLE endereco (
 idEndereco INT PRIMARY KEY AUTO_INCREMENT,
@@ -16,7 +9,9 @@ cidade VARCHAR(45) NOT NULL,
 bairro VARCHAR (45) NOT NULL,
 logradouro VARCHAR (45) NOT NULL,
 numero VARCHAR(45) NOT NULL,
-complemento VARCHAR(45));
+complemento VARCHAR(45)
+fkCliente INT NOT NULL,
+CONSTRAINT fkEnderecoCliente FOREIGN KEY (fkCliente) REFERENCES cliente(idCliente));
 
 CREATE TABLE cliente (
 idCliente INT PRIMARY KEY AUTO_INCREMENT,
@@ -24,9 +19,7 @@ nome_completo VARCHAR (45) NOT NULL,
 telefone VARCHAR(11) NOT NULL,
 cpf CHAR (11) NOT NULL,
 email VARCHAR (45) NOT NULL, 
-senha VARCHAR (45) NOT NULL, 
-fkEndereco INT NOT NULL,
-CONSTRAINT fkClienteEndereco FOREIGN KEY (fkEndereco) REFERENCES endereco(idEndereco));
+senha VARCHAR (45) NOT NULL);
 
 CREATE TABLE sensor (
 idSensor INT PRIMARY KEY AUTO_INCREMENT,
@@ -43,5 +36,5 @@ CREATE TABLE registro (
 idRegistro INT PRIMARY KEY AUTO_INCREMENT,
 dado FLOAT NOT NULL,
 dtColeta DATETIME DEFAULT CURRENT_TIMESTAMP,
-fkSensor INT DEFAULT 0,
+fkSensor INT DEFAULT 1,
 CONSTRAINT fkRegistroSensor FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor));
