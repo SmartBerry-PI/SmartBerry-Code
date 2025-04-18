1f92b7af2e211877537732d7a22e684570571999
davi-domingues <davi.domingues@sptech.school>
2025-04-18 01:02:00 -0300

    Ajuste de script de configuração do usuario

--- databaseScript/userConfig.sql
+++ databaseScript/userConfig.sql
@@ -2,8 +2,8 @@ CREATE USER 'api'@'%' IDENTIFIED BY 'Api#123.';
 
 GRANT INSERT ON SmartBerry.registro TO 'api'@'%';
 
-FLUSH PRIVILEGES;
-
 CREATE USER 'dev'@'%' IDENTIFIED BY 'Dev#123.';
 
 GRANT SELECT ON SmartBerry.* TO 'api'@'%';
+
+FLUSH PRIVILEGES;
