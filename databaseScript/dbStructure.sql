94f9a100fc36057b410ac873482124f86dd4e659
Arthur-Amaral-cloud <arthur.asilva@sptech.school>
2025-04-29 23:03:11 -0300

    Retrabalho na Modelagem e Adpatação das tabelas no script de Criação de tabelas

--- databaseScript/dbStructure.sql
+++ databaseScript/dbStructure.sql
@@ -5,43 +5,59 @@ CREATE TABLE usuario (
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR (45) NOT NULL,
 sobrenome VARCHAR(45) NOT NULL,
-username VARCHAR(45) NOT NULL,
+username VARCHAR(45) NOT NULL UNIQUE,
 TelefoneCelular VARCHAR(45) NOT NULL,
 cpf CHAR (11) NOT NULL,
 email VARCHAR (45) NOT NULL,
-senha VARCHAR(20) UNIQUE,
-fkEmpresa INT,
-CONSTRAINT fkEmpresa FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa));
+senha VARCHAR(20) UNIQUE
+);
+
+CREATE TABLE permissoes (
+FkEmpresa INT,
+FkUsuario INT,
+CONSTRAINT pkCompostaPermissoes PRIMARY KEY (FkEmpresa, FkUsuario),
+GerarUser TINYINT,
+CONSTRAINT chkGerarUser
+check(GerarUser in('0','1')),
+DeleteUser TINYINT,
+CONSTRAINT chkDeleUser
+check(DeleteUser in('0','1')),
+AcessarDashboard TINYINT,
+CONSTRAINT chkAcessarDashboard
+check(AcessarDashboard in('0','1')),
+AlterarPermissoes TINYINT,
+CONSTRAINT chkAlterarPermissoes
+check(AlterarPermissoes in('0','1'))
+);
 
 CREATE TABLE empresa (
 idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
 razaoSocial VARCHAR (45) NOT NULL,
-nomeFantasia VARCHAR (45) UNIQUE,
+nomeFantasia VARCHAR (45),
 inscEstadual VARCHAR(45) NOT NULL,
+CNPJ char(11) NOT NULL,
 nomepContato VARCHAR (45) NOT NULL,
 numeropContato VARCHAR (45) NOT NULL,
-emailpContato VARCHAR (45));
+emailpContato VARCHAR (45) NOT NULL);
 
 CREATE TABLE endereco (
 idEndereco INT,
 fkEmpresa INT,
 CONSTRAINT fkEnderecoEmpresa FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa),
 CONSTRAINT pkCompostaEndereco PRIMARY KEY (idEndereco, fkEmpresa),
-cep VARCHAR(45) NOT NULL,
+cep CHAR(8) NOT NULL,
 uf CHAR (2) NOT NULL,
 cidade VARCHAR(45) NOT NULL,
 bairro VARCHAR (45) NOT NULL,
 logradouro VARCHAR (45) NOT NULL,
-numero VARCHAR(5) NOT NULL,
-complemento VARCHAR(45) UNIQUE);
+numero VARCHAR(45) NOT NULL,
+complemento VARCHAR(45));
 
 CREATE TABLE sensor (
 idSensor INT AUTO_INCREMENT,
 fkEmpresa INT,
-fkEndereco INT,
-CONSTRAINT fkSensorEmpresa FOREIGN KEY sensor(fkEmpresa) REFERENCES endereco(fkEmpresa),
-CONSTRAINT fkSensorEndereco FOREIGN KEY sensor(fkEndereco) REFERENCES endereco(idEndereco),
-CONSTRAINT pkCompostaSensor PRIMARY KEY (idSensor, fkEmpresa, fkEndereco),
+CONSTRAINT fkSensorEmpresa FOREIGN KEY sensor(fkEmpresa) REFERENCES empresa(idEmpresa),
+CONSTRAINT pkCompostaSensor PRIMARY KEY (idSensor, fkEmpresa),
 nome VARCHAR(45) NOT NULL,
 modelo VARCHAR(45) NOT NULL, 
 situacao TINYINT NOT NULL,
