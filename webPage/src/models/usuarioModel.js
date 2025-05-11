65c4341ac4b8c5ae3df2a52ef6d987a1b4a6a21d
davi-domingues <davi.domingues@sptech.school>
2025-05-10 22:19:24 -0300

    Integrando função de cadastro pela api

--- webPage/src/models/usuarioModel.js
+++ webPage/src/models/usuarioModel.js
@@ -10,13 +10,11 @@ function autenticar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha, fkEmpresa) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
+function cadastrar(nome, sobrenome, email, senha, cpf, telefone, username) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, sobrenome, email, senha, cpf, telefone, username);
+
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${fkEmpresa}');
+        INSERT INTO usuario (nome, sobrenome, email, senha, cpf, telefoneCelular, username) VALUES ('${nome}', '${sobrenome}', '${email}', '${senha}', '${cpf}', '${telefone}', '${username}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
