c632f086d087c2a725e62fc82e5d264751a21c34
davi-domingues <davi.domingues@sptech.school>
2025-05-12 13:02:15 -0300

    Alterando inserts

--- databaseScript/insertsMock.sql
+++ databaseScript/insertsMock.sql
@@ -1,13 +1,12 @@
-INSERT INTO SmartBerry.cliente VALUE
-	(1,'João da Silva', '11987654321', '12345678901111', 'joao.silva@email.com');
-    
-INSERT INTO SmartBerry.usuario VALUES
-	(1, 1, 'jo.silva', 'senhasegura123', 'A'),
-	(2, 1, 'ge.silva', 'senhasegura123', 'S'),
-	(3, 1, 'lu.neves', 'senhasegura123', 'U');
+INSERT INTO usuario VALUE (1000, 'João', 'Silva', 'jo_silva', '12345678910', '11999998888', 'joao.silva@email.com', 'senha@segura123');
 
-INSERT INTO SmartBerry.endereco VALUE
-	(1, 1, '01001-000', 'SP', 'São Paulo', 'Sé', 'Praça da Sé', '100', NULL);
-    
-INSERT INTO SmartBerry.sensor VALUE
-	(1, 1, 'Sensor 1', 'Capacitivo de Umidade de Solo v2.0', 1, 'canteiro 1', '2025-01-01');
+INSERT INTO empresa VALUE (1000, 'JoSil.agrofarm', 'Recanto do Morango', '110.042.490.140', '12345678000190');
+
+INSERT INTO endereco VALUE (1000, 1000, '12345678', 'SP', 'São Roque', 'Centro', 'Av. Brasil', '106');
+
+INSERT INTO canteiro VALUE (1000, 1000, 'Canteiro 1A', current_timestamp);
+
+INSERT INTO sensor VALUES
+	(1000, 1000, 1000, 'Sensor 1', 1, current_timestamp),
+	(1001, 1000, 1000, 'Sensor 2', 1, current_timestamp),
+	(1002, 1000, 1000, 'Sensor 3', 1, current_timestamp);
